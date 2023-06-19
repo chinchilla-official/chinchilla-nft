32d96a9c8df51eb71a5ff62961825a29e1550465
chinchilla <chinchilla.marketing.2023@gmail.com>
2023-06-19 16:58:25 +0800

    init

---
+++ dapp.config.js
@@ -0,0 +1,60 @@
+const FORTMATIC_KEY = process.env.NEXT_PUBLIC_FORTMATIC_KEY
+const RPC_URL = process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL
+
+const config = {
+  title: 'Chinchilla Finance',
+  description: 'A showcase dapp which is built for you to create your own',
+  // contractAddress: '0x669e54721a533329db7388584e84c714f5f8db03',
+  contractAddress: '0x12529194219ddcc39c162a55583db8c06069d6ef',
+  maxMintAmount: 2,
+  presaleMaxMintAmount: 3,
+  price: 0.001
+}
+
+const onboardOptions = {
+  dappId: process.env.NEXT_PUBLIC_DAPP_ID,
+  networkId: 42161, // Rinkeby
+  darkMode: true,
+  walletSelect: {
+    wallets: [
+      { walletName: 'metamask', preferred: true },
+      { walletName: 'coinbase', preferred: true },
+      {
+        walletName: 'walletLink',
+        preferred: true,
+        rpcUrl: RPC_URL,
+        appName: 'BoredApe Dapp'
+      },
+      {
+        walletName: 'fortmatic',
+        apiKey: FORTMATIC_KEY,
+        preferred: true
+      },
+      { walletName: 'trust', preferred: true, rpcUrl: RPC_URL },
+      { walletName: 'gnosis', preferred: true },
+      { walletName: 'authereum' },
+
+      {
+        walletName: 'ledger',
+        rpcUrl: RPC_URL
+      },
+      {
+        walletName: 'lattice',
+        rpcUrl: RPC_URL,
+        appName: 'BoredApe Dapp'
+      },
+      {
+        walletName: 'keepkey',
+        rpcUrl: RPC_URL
+      }
+    ]
+  },
+  walletCheck: [
+    { checkName: 'derivationPath' },
+    { checkName: 'accounts' },
+    { checkName: 'connect' },
+    { checkName: 'network' }
+  ]
+}
+
+export { config, onboardOptions }
